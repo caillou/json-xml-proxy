6646df15089a0e2318604bf50b4c0a3204829fb2
Pierre Spring <pierre@nelm.io>
2015-11-19 15:42:24 +0100

    minimal setup

--- index.js
+++ index.js
@@ -3,18 +3,12 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.send(200);
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
